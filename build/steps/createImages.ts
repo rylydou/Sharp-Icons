96d3153ce7b70655e12a720ae548608ba60bc1a8
CiberTurtle <rylydou@gmail.com>
2023-01-08 16:08:21 -0600

    WIP: convert svg to png

--- build/steps/createImages.ts
+++ build/steps/createImages.ts
@@ -2,8 +2,17 @@ import * as fs from 'fs'
 import path = require('path')
 import { fileIcons } from '../icons/fileIcons'
 import { create } from 'xmlbuilder2'
+import sharp from 'sharp'
 
 // the maximum width of an image is 882px in vscode
+
+declare type CollageOptions = {
+	padding: 4
+	spacing: 16
+	iconSize: 16
+	reuseIcons: true
+}
+
 export default function createImages() {
 	console.log('--- creating images ---')
 
@@ -11,14 +20,16 @@ export default function createImages() {
 
 	const icons = fs.readdirSync('icons')
 
-	function createIconCollage(outputPath: string, colCount: number, rowCount?: number, padding = 8.0, spacing = 16.0, iconSize = 16.0, reuseIcons = true) {
+	function createIconCollage(outputPath: string, colCount: number, rowCount?: number, options?: CollageOptions) {
+		options = options || new CollageOptions()
+
 		rowCount = rowCount || Math.ceil(icons.length / colCount)
 
 		const area = colCount * rowCount
 		const diff = area - icons.length
 		const originalLength = icons.length
 
-		if (reuseIcons) {
+		if (options.reuseIcons) {
 			// fill the the remaining space by reusing icons
 			console.log(`  - reusing ${diff} icons`)
 			for (let index = 0; index < diff; index++) {
@@ -27,8 +38,8 @@ export default function createImages() {
 			}
 		}
 
-		const imageWidth = (colCount * (iconSize + spacing) - spacing) + (padding * 2)
-		const imageHeight = (rowCount * (iconSize + spacing) - spacing) + (padding * 2)
+		const imageWidth = (colCount * (options.iconSize + options.spacing) - options.spacing) + (options.padding * 2)
+		const imageHeight = (rowCount * (options.iconSize + options.spacing) - options.spacing) + (options.padding * 2)
 
 		const imageDoc = create(`<svg width="${imageWidth}" height="${imageHeight}" viewBox="0 0 ${imageWidth} ${imageHeight}" fill="none" xmlns="http://www.w3.org/2000/svg"></svg>`)
 
@@ -37,8 +48,8 @@ export default function createImages() {
 			const col = index % colCount
 			const row = Math.floor(index / colCount)
 
-			const x = padding + (col * (iconSize + spacing))
-			const y = padding + (row * (iconSize + spacing))
+			const x = options.padding + (col * (options.iconSize + options.spacing))
+			const y = options.padding + (row * (options.iconSize + options.spacing))
 
 			const iconSvg = fs.readFileSync(path.join('icons', icon)).toString()
 
@@ -51,11 +62,17 @@ export default function createImages() {
 
 		let imageSvg = imageDoc.end({ prettyPrint: true })
 		imageSvg = imageSvg.replace('<?xml version="1.0"?>\n', '')
-		fs.writeFileSync(outputPath, imageSvg)
+
+		sharp(Buffer.from(imageSvg))
+			.resize(imageWidth * 2)
+			.png()
+			.toBuffer()
+			.then(buf => fs.writeFileSync(outputPath, buf))
+			.catch(reason => console.error(reason))
 	}
 
 	console.log('making banner...')
-	createIconCollage(path.join('images', 'readme', 'banner.svg'), 27)
+	createIconCollage(path.join('images', 'readme', 'banner.png'), 14, undefined, { padding: 6 })
 	// createIconCollage(path.join('images', 'readme', 'icons.svg'), 9)
 }
 
