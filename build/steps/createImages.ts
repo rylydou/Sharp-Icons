ef83649f64c43bd4fbd0a33761a2217595235bd9
Ciber_Turtle <rylydou@gmail.com>
2023-01-08 20:38:20 -0600

    WIP: readme images

--- build/steps/createImages.ts
+++ build/steps/createImages.ts
@@ -6,11 +6,11 @@ import sharp from 'sharp'
 
 // the maximum width of an image is 882px in vscode
 
-declare type CollageOptions = {
-	padding: 4
-	spacing: 16
-	iconSize: 16
-	reuseIcons: true
+type CollageOptions = {
+	iconSize: number
+	padding: number
+	spacing: number
+	reuseIcons: boolean
 }
 
 export default function createImages() {
@@ -20,16 +20,23 @@ export default function createImages() {
 
 	const icons = fs.readdirSync('icons')
 
-	function createIconCollage(outputPath: string, colCount: number, rowCount?: number, options?: CollageOptions) {
-		options = options || new CollageOptions()
+	function createIconCollage(colCount: number, options: Partial<CollageOptions> = {}): sharp.Sharp {
+		const defaults: CollageOptions = {
+			iconSize: 16,
+			padding: 4,
+			spacing: 16,
+			reuseIcons: false,
+		}
 
-		rowCount = rowCount || Math.ceil(icons.length / colCount)
+		const opts = Object.assign(defaults, options)
+
+		const rowCount = Math.ceil(icons.length / colCount)
 
 		const area = colCount * rowCount
 		const diff = area - icons.length
 		const originalLength = icons.length
 
-		if (options.reuseIcons) {
+		if (opts.reuseIcons) {
 			// fill the the remaining space by reusing icons
 			console.log(`  - reusing ${diff} icons`)
 			for (let index = 0; index < diff; index++) {
@@ -38,8 +45,8 @@ export default function createImages() {
 			}
 		}
 
-		const imageWidth = (colCount * (options.iconSize + options.spacing) - options.spacing) + (options.padding * 2)
-		const imageHeight = (rowCount * (options.iconSize + options.spacing) - options.spacing) + (options.padding * 2)
+		const imageWidth = (colCount * (opts.iconSize + opts.spacing) - opts.spacing) + (opts.padding * 2)
+		const imageHeight = (rowCount * (opts.iconSize + opts.spacing) - opts.spacing) + (opts.padding * 2)
 
 		const imageDoc = create(`<svg width="${imageWidth}" height="${imageHeight}" viewBox="0 0 ${imageWidth} ${imageHeight}" fill="none" xmlns="http://www.w3.org/2000/svg"></svg>`)
 
@@ -48,8 +55,8 @@ export default function createImages() {
 			const col = index % colCount
 			const row = Math.floor(index / colCount)
 
-			const x = options.padding + (col * (options.iconSize + options.spacing))
-			const y = options.padding + (row * (options.iconSize + options.spacing))
+			const x = opts.padding + (col * (opts.iconSize + opts.spacing))
+			const y = opts.padding + (row * (opts.iconSize + opts.spacing))
 
 			const iconSvg = fs.readFileSync(path.join('icons', icon)).toString()
 
@@ -63,16 +70,25 @@ export default function createImages() {
 		let imageSvg = imageDoc.end({ prettyPrint: true })
 		imageSvg = imageSvg.replace('<?xml version="1.0"?>\n', '')
 
-		sharp(Buffer.from(imageSvg))
-			.resize(imageWidth * 2)
-			.png()
-			.toBuffer()
-			.then(buf => fs.writeFileSync(outputPath, buf))
-			.catch(reason => console.error(reason))
+		return sharp(Buffer.from(imageSvg))
 	}
 
 	console.log('making banner...')
-	createIconCollage(path.join('images', 'readme', 'banner.png'), 14, undefined, { padding: 6 })
+	createIconCollage(14, { padding: 9, reuseIcons: true })
+		.resize((14 * 32 - 16 + 9) * 2, null, { fit: 'inside' })
+		.composite([
+			{
+				input: { create: { background: 'white', width: 600, height: 165, channels: 3 } },
+				blend: 'dest-out',
+			},
+			{
+				input: Buffer.from(fs.readFileSync(path.join('images', 'logo.png'))),
+			}
+		])
+		.png()
+		.toBuffer()
+		.then(buf => fs.writeFileSync(path.join('images', 'readme', 'banner.png'), buf))
+		.catch(reason => console.error(reason))
 	// createIconCollage(path.join('images', 'readme', 'icons.svg'), 9)
 }
 
